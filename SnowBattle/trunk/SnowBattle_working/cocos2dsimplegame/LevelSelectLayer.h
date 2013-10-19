df9f67ceef76a8955cb62dddbd47dcb662306399
Akshay <akshayrd@usc.edu>
2013-10-19 14:09:04 -0700

    Revert "Revert d6c7a5e..24055fa"
    This reverts commit 420d52b962b7229f8841960928e09ac0c0617129.
    
    Conflicts:
    
    SnowBattle/trunk/SnowBattle_working/cocos2dsimplegame.xcodeproj/project.
    pbxproj
    
    SnowBattle/trunk/SnowBattle_working/cocos2dsimplegame.xcodeproj/project.
    xcworkspace/xcuserdata/Akshay.xcuserdatad/UserInterfaceState.xcuserstate
    
    SnowBattle/trunk/SnowBattle_working/cocos2dsimplegame.xcodeproj/project.
    xcworkspace/xcuserdata/gpadmin.xcuserdatad/UserInterfaceState.xcuserstat
    e
    
    SnowBattle/trunk/SnowBattle_working/cocos2dsimplegame.xcodeproj/xcuserda
    ta/Akshay.xcuserdatad/xcdebugger/Breakpoints_v2.xcbkptlist
    	SnowBattle/trunk/SnowBattle_working/cocos2dsimplegame/GameStartLayer.m
    	SnowBattle/trunk/SnowBattle_working/cocos2dsimplegame/HelloWorldLayer.h
    	SnowBattle/trunk/SnowBattle_working/cocos2dsimplegame/HelloWorldLayer.m
    
    SnowBattle/trunk/SnowBattle_working/cocos2dsimplegame/LevelSelectLayer.h
    
    SnowBattle/trunk/SnowBattle_working/cocos2dsimplegame/LevelSelectLayer.m
    	SnowBattle/trunk/SnowBattle_working/cocos2dsimplegame/level.png
    	SnowBattle/trunk/SnowBattle_working/cocos2dsimplegame/levelscreen.png

---
+++ SnowBattle/trunk/SnowBattle_working/cocos2dsimplegame/LevelSelectLayer.h
@@ -0,0 +1,19 @@
+//
+//  LevelSelectLayer.h
+//  cocos2dsimplegame
+//
+//  Created by Student on 10/15/13.
+//  Copyright 2013 gpadmin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface LevelSelectLayer : CCLayer {
+    
+}
+
++(id) firstScene:(BOOL)start;
+- (id)initW:(BOOL)start;
+
+@end
